c4b848bc128b2bd6a8b1ec9edfdaa44caea855d0
pardhusnc2004 <pardupolisetty2018@gmail.com>
2024-10-02 23:58:45 +0530

    welcome to pushncode!

---
+++ welcome.py
@@ -0,0 +1,18 @@
+import time
+
+def print_welcome():
+    print(r"""
+    =========================================
+            Welcome to pushncode!        
+    =========================================
+    """)
+    
+    print("Get ready to code and push daily!\n")
+    
+    print("Let's start this journey of commits, code, and creativity! 🚀")
+    print("Each day is a new opportunity to learn and grow. 💡")
+    print("\nHappy Coding! Let's make today count! 💻\n")
+    
+
+if __name__ == "__main__":
+    print_welcome()
