2de71a0a3ba442f1b98ba1cec5d3a16ef87087dd
pardhusnc2004 <pardupolisetty2018@gmail.com>
2025-03-29 20:32:00 +0530

    commit on 29 March 2025

---
+++ 2025/03. March/29/GeeksforGeeks_29-03.py
@@ -0,0 +1,28 @@
+'''
+    GeeksforGeeks Daily Question (29-03-2025)
+    Job Sequencing Problem
+    Python3 solution
+'''
+
+from heapq import *
+
+class Solution:
+    def jobSequencing(self, deadline, profit):
+        # code here
+        jobs = list(zip(deadline, profit))
+        jobs.sort()
+        max_profit = 0
+        job_count = 0
+        max_heap = []
+        index = len(jobs) - 1
+        
+        for i in range(jobs[-1][0], 0, -1):
+            while index >= 0 and jobs[index][0] == i:
+                heappush(max_heap, -jobs[index][1]) 
+                index -= 1
+            
+            if max_heap:
+                job_count += 1
+                max_profit += -heappop(max_heap)
+        
+        return [job_count, max_profit]
---
+++ 2025/03. March/29/Leetcode_2818.py
@@ -0,0 +1,15 @@
+'''
+    Leetcode Daily Question (29-03-2025)
+    2818. Apply Operations to Maximize Score
+    Python3 solution
+'''
+from typing import List
+from collections import *
+from bisect import *
+from math import *
+from heapq import *
+
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        # HaHa.... I didn't solve it today... 
+        return 
